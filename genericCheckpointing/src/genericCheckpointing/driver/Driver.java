efd1c966b7ffeec834301b49850d835fdb4c1300
Sujoy Das <sdas4@binghamton.edu>
2018-05-05 16:59:00 -0400

    minor changes

--- genericCheckpointing/src/genericCheckpointing/driver/Driver.java
+++ genericCheckpointing/src/genericCheckpointing/driver/Driver.java
@@ -43,33 +43,34 @@ public class Driver {
 			// serialization starts
 			processor.openFileToWrite(filename);
 			handler.setFileprocessor(processor);
-			randomValueGenerator(rand, serobjList, srObject, noOfObjects);
+			util.randomValueGenerator(rand, serobjList, srObject, noOfObjects);
 			processor.closeFile();
 			// serialization ends
+			
+			// Print all objects created
+			System.out.println("\nRandom generated objects");
+			for (SerializableObject obj : serobjList) {
+				System.out.println(obj);
+			}
 			
 			// Deserialzaton starts
 			processor.openFile(filename);
 			handler.setFileprocessor(processor);
-			System.out.println("random generated objects");
+			System.out.println("\nObjects after deserialization");
 			((RestoreI) srObject).readObj("XML");
 			processor.closeFile();
 			// Deserialzaton ends
 
-			// Print all objects created
-			serReadObj.addAll(processor.getSerReadObj());
-			System.out.println("Objects after deserialization");
-			for (SerializableObject obj : serobjList) {
-				System.out.println(obj);
-			}
-
+			
 			// counting mismatch
+			serReadObj.addAll(processor.getSerReadObj());
 			int mismatchCount = 0;
 			for (int j = 0; j < serobjList.size(); j++) {
 				if (!serobjList.get(j).equals(serReadObj.get(j))) {
 					mismatchCount++;
 				}
 			}
-			System.out.println("Mismatch Count is: " + mismatchCount);
+			System.out.println("\nMismatch Count is: " + mismatchCount);
 		} else {
 			System.out.println("No such mode");
 			processor.closeFile();
@@ -77,39 +78,6 @@ public class Driver {
 		}
 		processor.closeFile();
 
-	}
-
-	private static void randomValueGenerator(Random rand, List<SerializableObject> serobjList, StoreRestoreI srObject,
-			int noOfObjects) {
-		MyAllTypesFirst myFirst;
-		MyAllTypesSecond mySecond;
-		char myChar = 'A';
-		// noOfObjects:number of objects to be deserialized
-		for (int i = 0; i < noOfObjects; i++) {
-			// random value generation logic
-			int value = rand.nextInt(20)+1;
-			int myInt = value;
-			int myOtherInt = value;
-			long myLong = (long) value;
-			long myOtherLong = (long) value;
-			String myString = "Design patterns" + i;
-			boolean myBool = Math.random() < 0.5;
-
-			myFirst = new MyAllTypesFirst(myInt, myOtherInt, myLong, myOtherLong, myString, myBool);
-			((StoreI) srObject).writeObj(myFirst, 1, "XML");
-			serobjList.add(myFirst);
-
-			double val = 20 * rand.nextDouble();
-			double myDouble = val;
-			double myOtherDouble = val;
-			float myFloat =(float) val;
-			short myShort = (short) val;
-			myChar += 1;
-
-			mySecond = new MyAllTypesSecond(myDouble, myOtherDouble, myFloat, myShort, myShort, myChar);
-			((StoreI) srObject).writeObj(mySecond, 2, "XML");
-			serobjList.add(mySecond);
-		}
 	}
 
 }
--- genericCheckpointing/src/genericCheckpointing/util/Utility.java
+++ genericCheckpointing/src/genericCheckpointing/util/Utility.java
@@ -1,5 +1,11 @@
 package genericCheckpointing.util;
 
+import java.util.List;
+import java.util.Random;
+
+import genericCheckpointing.server.StoreI;
+import genericCheckpointing.server.StoreRestoreI;
+
 public class Utility {
 	public void validateFile(String[] args) {
 		try {
@@ -10,6 +16,40 @@ public class Utility {
 		} catch (Exception e) {
 			System.err.println("File not found");
 			System.exit(1);
+		}
+	}
+	
+
+	public void randomValueGenerator(Random rand, List<SerializableObject> serobjList, StoreRestoreI srObject,
+			int noOfObjects) {
+		MyAllTypesFirst myFirst;
+		MyAllTypesSecond mySecond;
+		char myChar = 'A';
+		// noOfObjects:number of objects to be deserialized
+		for (int i = 0; i < noOfObjects; i++) {
+			// random value generation logic
+			int value = rand.nextInt(20)+1;
+			int myInt = value;
+			int myOtherInt = value;
+			long myLong = (long) value;
+			long myOtherLong = (long) value;
+			String myString = "Design patterns" + i;
+			boolean myBool = Math.random() < 0.5;
+
+			myFirst = new MyAllTypesFirst(myInt, myOtherInt, myLong, myOtherLong, myString, myBool);
+			((StoreI) srObject).writeObj(myFirst, 1, "XML");
+			serobjList.add(myFirst);
+
+			double val = 20 * rand.nextDouble();
+			double myDouble = val;
+			double myOtherDouble = val;
+			float myFloat =(float) val;
+			short myShort = (short) val;
+			myChar += 1;
+
+			mySecond = new MyAllTypesSecond(myDouble, myOtherDouble, myFloat, myShort, myShort, myChar);
+			((StoreI) srObject).writeObj(mySecond, 2, "XML");
+			serobjList.add(mySecond);
 		}
 	}
 }
